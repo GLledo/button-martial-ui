23e838573c35f13b8d6adc002f7be63d441ecfaf
Gonzalo lledo <gonzalo_lledo@outlook.com>
2021-10-20 14:27:10 +0200

    the material ui

--- src/components/ButtonDev.js
+++ src/components/ButtonDev.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import Button from '@mui/material/Button'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 
- const ButtonDev = ({ nameButon, text, variantButton }) =>{
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#878787',
+    },
+    secondary: {
+      main: '#1565c0',
+    },
+  },
+});
+
+ const ButtonDev = ({ nameButon, variantButton, colorBg }) =>{
     return (
-        <>
-            <Button type="default"  size="large"  variant={variantButton} >{nameButon}</Button>
-        </>
+        <ThemeProvider theme={theme}>
+            <Button type="default"  size="large" color={colorBg}  variant={variantButton} >{nameButon}</Button>
+        </ThemeProvider>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ButtonDev from './ButtonDev'
+import './Home.css'
 
 export default function Home() {
     return (
         <div>
             <div>
-                <p>{'\u0062'} Button  & </p>
+                <p>{'\u0062'} Button </p>
                 <ButtonDev text="<button />" nameButon="Default" variantButton="contained" />  
             </div>
         </div>
